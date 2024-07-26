1ab45c25a2d7c9fed071a0ff35d1c565c1ff4f8b
Dmimaz <korvin.ambr@gmail.com>
2024-07-26 10:26:04 +0300

    refactoring

--- start.sh
+++ start.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 
-for (( ; ; ))
-do
-    file="phrases.txt"
-    # file="debug_phrases.txt"
-    url="https://0x0aa110d2e3a2f14fc122c849cea06d1bc9ed1c62.us.gaianet.network/v1/chat/completions"
+# File containing phrases
+file="phrases.txt"
+# Alternative debug file
+# file="debug_phrases.txt"
 
-    while read -r line; do
-        curl -X OST $url \
+# URL for the API
+url="https://0x0aa110d2e3a2f14fc122c849cea06d1bc9ed1c62.us.gaianet.network/v1/chat/completions"
+
+# Function to send the API request
+send_request() {
+    local line=$1
+    curl -X POST "$url" \
         -H 'accept: application/json' \
         -H 'Content-Type: application/json' \
         -d "{\"messages\":[{\"role\":\"system\", \"content\": \"You are a helpful assistant.\"}, {\"role\":\"user\", \"content\": \"$line\"}]}"
-        echo -e "\n"
-        sleep 1
-    done < $file 
+    echo -e "\n"
+}
+
+# Infinite loop to repeatedly read the file and send requests
+while true; do
+    if [[ -f $file ]]; then
+        while IFS= read -r line; do
+            send_request "$line"
+            sleep 1
+        done < "$file"
+    else
+        echo "File '$file' not found. Retrying in 5 seconds..."
+        sleep 5
+    fi
 done
